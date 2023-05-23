cea1be4b2f65d3e2cccf995143789e5b63de910a
Iamswart <swartjide@gmail.com>
2023-05-23 22:15:45 +0100

    Manage state with zustand

--- src/components/GameHeading.tsx
+++ src/components/GameHeading.tsx
@@ -1,24 +1,23 @@
-import { Heading } from '@chakra-ui/react'
-import React from 'react'
-import { GameQuery } from '../App'
-import useGenres from '../hooks/useGenres'
-import usePlatform from '../hooks/usePlatform'
-import useGenre from '../hooks/useGenre'
+import { Heading } from "@chakra-ui/react";
+import React from "react";
+import useGenres from "../hooks/useGenres";
+import usePlatform from "../hooks/usePlatform";
+import useGenre from "../hooks/useGenre";
+import useGameQueryStore from "../store";
 
-interface Props {
-    gameQuery: GameQuery
-}
-
-
-const GameHeading = ( {gameQuery }: Props) => {
-  const genre = useGenre(gameQuery.genreId)
+const GameHeading = () => {
+  const genreId = useGameQueryStore((s) => s.gameQuery.genreId);
+  const genre = useGenre(genreId);
   
-  const platform = usePlatform(gameQuery.platformId)
+  const platformId = useGameQueryStore((s) => s.gameQuery.platformId);
+  const platform = usePlatform(platformId);
 
-    const heading = `${platform?.name || ''} ${genre?.name || ''} Games`
+  const heading = `${platform?.name || ""} ${genre?.name || ""} Games`;
   return (
-    <Heading as='h1' marginY={5} fontSize='4xl'>{heading}</Heading>
-  )
-}
+    <Heading as="h1" marginY={5} fontSize="4xl">
+      {heading}
+    </Heading>
+  );
+};
 
-export default GameHeading
+export default GameHeading;
--- src/components/SortSelector.tsx
+++ src/components/SortSelector.tsx
@@ -1,14 +1,11 @@
 import { Button, Menu, MenuButton, MenuItem, MenuList } from '@chakra-ui/react'
-import React from 'react'
 import { BsChevronDown } from 'react-icons/bs'
-
-interface Props {
-    onSelectSortOrder: (sortOrder: string) => void;
-    sortOrder: string;
-}
+import useGameQueryStore from '../store'
 
 
-const SortSelector = ( {onSelectSortOrder, sortOrder }: Props) => {
+
+
+const SortSelector = () => {
 
     const sortOrders = [
         { value: '', label: 'Relevance'},
@@ -19,6 +16,8 @@ const SortSelector = ( {onSelectSortOrder, sortOrder }: Props) => {
         { value: '-rating', label: 'Average Rating'},
     ]
 
+    const setSortOrder = useGameQueryStore(s => s.setSortOrder)
+    const sortOrder = useGameQueryStore(s => s.gameQuery.sortOrder)
     const currentSortOrder = sortOrders.find(order => order.value === sortOrder)
     return (
         <Menu>
@@ -26,7 +25,7 @@ const SortSelector = ( {onSelectSortOrder, sortOrder }: Props) => {
             Order by: {currentSortOrder?.label || 'Relevance'}
           </MenuButton>
           <MenuList>
-            {sortOrders.map(order => <MenuItem onClick={() => onSelectSortOrder(order.value)} key={order.value} value={order.value}>{order.label}</MenuItem>)}
+            {sortOrders.map(order => <MenuItem onClick={() => setSortOrder(order.value)} key={order.value} value={order.value}>{order.label}</MenuItem>)}
           </MenuList>
         </Menu>
       )
